f261fee6fe04b4f91195070ebe53d1aefe3e2c8f
Imann7 <imoallahverdiyev14@gmail.com>
2023-05-01 20:06:11 +0200

    main

--- src/Map/Map.jsx
+++ src/Map/Map.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import GoogleMapReact from "google-map-react";
 import { MdLocationOn } from "react-icons/md";
+import mapStyles from "./mapStyles";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 
+const Aicon = ({ icon }) => (
+  <div>
+    <FontAwesomeIcon icon={icon} />
+  </div>
+);
 const Map = ({ setCoordinates, setBounds, coordinates, places }) => {
+  const [location, setLocation] = useState(null);
+
+  const handleMapClick = (event) => {
+    setLocation({
+      lat: event.lat,
+      lng: event.lng,
+    });
+  };
+
   return (
     <div
       style={{ marginTop: "25px", height: "91vh", width: "900px" }}
@@ -13,16 +30,27 @@ const Map = ({ setCoordinates, setBounds, coordinates, places }) => {
         defaultCenter={coordinates}
         center={coordinates}
         defaultZoom={11}
+        options={{
+          disableDefaultUI: true,
+          zoomControl: true,
+          styles: mapStyles,
+        }}
         onChange={(e) => {
           setCoordinates({ lat: e.center.lat, lng: e.center.lng });
           setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
         }}
+        onClick={handleMapClick}
       >
         {places?.map((place, i) => (
           <div lat={place.latitude} lng={place.longitude} key={i}>
-            <MdLocationOn />
+            <Aicon icon={faMapMarkerAlt} />
           </div>
         ))}
+        {location && (
+          <div lat={location.lat} lng={location.lng}>
+            <Aicon icon={faMapMarkerAlt} />
+          </div>
+        )}
       </GoogleMapReact>
     </div>
   );
